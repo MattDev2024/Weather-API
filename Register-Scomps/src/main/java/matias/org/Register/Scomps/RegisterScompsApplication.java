512d0bd0d381d06da415463194889adfa1e3a5a5
MattDev2024 <matheus.ram.lima@gmail.com>
2024-07-06 02:06:10 -0300

    atualizando core

---
+++ Register-Scomps/src/main/java/matias/org/Register/Scomps/RegisterScompsApplication.java
@@ -0,0 +1,13 @@
+package matias.org.Register.Scomps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterScompsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterScompsApplication.class, args);
+	}
+
+}
---
+++ Register-Scomps/src/test/java/matias/org/Register/Scomps/RegisterScompsApplicationTests.java
@@ -0,0 +1,13 @@
+package matias.org.Register.Scomps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterScompsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
